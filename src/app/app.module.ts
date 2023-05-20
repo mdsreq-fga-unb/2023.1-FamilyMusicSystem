7a9e68e6dd081be9cc658c327c118f77d3fb8c4a
maykonjuso <maykola1331@gmail.com>
2023-05-20 10:30:01 -0300

    update: removendo arquivos padrão angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppTeste } from './app.teste';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppTeste],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppTeste],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.teste.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.teste.html',
+})
+export class AppTeste {
+  title = 'teste';
+}
