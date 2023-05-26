36992b42faf2b6c152d415eb9a44944de5fc938b
maykonjuso <maykola1331@gmail.com>
2023-05-25 21:55:12 -0300

    add: adicionando todas as rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { LoginComponent } from './modules/login/login.component';
 import { SidebarComponent } from './template/sidebar/sidebar.component';
 import { FooterComponent } from './template/footer/footer.component';
 import { HeaderComponent } from './template/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MainComponent } from './modules/main/main.component';
 import { StudentsRegisterComponent } from './modules/student/students-register/students-register.component';
 import { StudentsListComponent } from 'src/app/modules/student/students-list/students-list.component';
 import { TeachersListComponent } from './modules/teacher/teachers-list/teachers-list.component';
 import { TeachersRegisterComponent } from './modules/teacher/teachers-register/teachers-register.component';
+import { HomeComponent } from './modules/home/home.component';
+import { MuralComponent } from './modules/mural/mural.component';
+import { RoomsComponent } from './modules/rooms/rooms.component';
+import { ScheduleComponent } from './modules/schedule/schedule.component';
+import { SettingsComponent } from './modules/settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +27,16 @@ import { TeachersRegisterComponent } from './modules/teacher/teachers-register/t
     SidebarComponent,
     FooterComponent,
     HeaderComponent,
-    ButtonComponent,
     MainComponent,
     StudentsListComponent,
     StudentsRegisterComponent,
     TeachersListComponent,
     TeachersRegisterComponent,
+    HomeComponent,
+    MuralComponent,
+    RoomsComponent,
+    ScheduleComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
