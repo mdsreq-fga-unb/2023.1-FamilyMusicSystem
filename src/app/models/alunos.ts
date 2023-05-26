32f22eb9475634880aa684a9127676c527245759
guinuto <guiguievangelista18@gmail.com>
2023-05-26 18:16:25 -0300

    add : adicionando models

---
+++ src/app/models/alunos.ts
@@ -0,0 +1,14 @@
+
+
+export class Alunos {
+    cpf : string;
+    nome : string; 
+    sobrenome: string;
+    dataDeNascimento : string;
+    rg : string;
+    pcd : string;
+    tipoDeDeficiencia? : string;
+    genero: string;
+
+    
+}
