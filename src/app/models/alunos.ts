649b1716a1046686c324bf29097f0ebe78ebd378
davirany <davirany@gmail.com>
2023-05-27 00:19:04 -0300

    merge: crud into desenvolvimento

--- src/app/models/alunos.ts
+++ src/app/models/alunos.ts
@@ -1,14 +1,10 @@
-
-
 export class Alunos {
-    cpf : string;
-    nome : string; 
+    cpf: string;
+    nome: string;
     sobrenome: string;
-    dataDeNascimento : string;
-    rg : string;
-    pcd : string;
-    tipoDeDeficiencia? : string;
+    dataDeNascimento: string;
+    rg: string;
+    pcd: string;
+    tipoDeDeficiencia?: string;
     genero: string;
-
-    
 }
--- src/app/models/professor.ts
+++ src/app/models/professor.ts
@@ -1,11 +1,10 @@
-export class Professores{
-    cpf : string;
-    nome : string; 
+export class Professores {
+    cpf: string;
+    nome: string;
     sobrenome: string;
-    dataDeNascimento : string;
-    rg : string;
-    pcd : string;
-    tipoDeDeficiencia? : string;
+    dataDeNascimento: string;
+    rg: string;
+    pcd: string;
+    tipoDeDeficiencia?: string;
     genero: string;
-
 }
