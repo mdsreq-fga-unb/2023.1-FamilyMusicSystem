34398db06ba7fef8dfff06f0e43d55074fec6d42
Luan Mateus <luanmateus.duarte@gmail.com>
2023-05-31 21:09:11 -0300

    update: atualizando modal de cadastro de salas de aula em tela responsiva

--- frontend/src/app/modules/rooms/rooms-register/rooms-register.component.ts
+++ frontend/src/app/modules/rooms/rooms-register/rooms-register.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Rooms } from 'src/app/models/rooms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-rooms-register',
   templateUrl: './rooms-register.component.html',
   styleUrls: ['./rooms-register.component.scss']
 })
-export class RoomsRegisterComponent {
-  private bsModalRef : BsModalRef;
+export class RoomsRegisterComponent implements OnInit {
+  public rooms: Rooms;
+  public onClose: Subject<boolean>;
+  public edicao = false;
+  public inicial = true;
+  public classForm: FormGroup;
+
+  constructor(private bsModalRef: BsModalRef, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.classForm = this.fb.group({
+      className: [null, Validators.required],
+      classDescription: [null, Validators.required],
+      classCapacity: [null, Validators.required],
+      classNumber: [null, Validators.required],
+      classLocal: [null, Validators.required],
+    });
+  }
+
+  scrollTop() {
+    const div = document.getElementById('scroll');
+    if (div !== null) {
+      div.scrollTop = 0;
+    }
+  }
+
+  sair() {
+    this.bsModalRef.hide();
+  }
 }
