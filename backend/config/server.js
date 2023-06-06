7a620f525a1d3d3dfe90a7bbc7505339770a1ab4
maykonjuso <maykola1331@gmail.com>
2023-06-06 10:27:54 -0300

    fix: corrigindo arquivos de testes 2

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,16 +1,10 @@
-require("dotenv").config();
-
 module.exports = ({ env }) => ({
-  host: process.env.HOST || env("HOST", "0.0.0.0"),
-  port: process.env.PORT || env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys:
-      process.env.APP_KEYS.split(",").map((key) => key.trim()) ||
-      env.array("APP_KEYS"),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
-    populateRelations:
-      process.env.WEBHOOKS_POPULATE_RELATIONS ||
-      env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
