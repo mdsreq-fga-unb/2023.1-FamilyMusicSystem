ccb7391e119c1d51a46b048cafe27ece4f753a3e
guinuto <guiguievangelista18@gmail.com>
2023-06-10 00:42:00 -0300

    update: testes professor

--- frontend/src/app/modules/teacher/teachers-list/teachers-list.component.spec.ts
+++ frontend/src/app/modules/teacher/teachers-list/teachers-list.component.spec.ts
@@ -1,25 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { TeachersListComponent } from './teachers-list.component';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TeachersListComponent', () => {
   let component: TeachersListComponent;
   let fixture: ComponentFixture<TeachersListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [TeachersListComponent],
       providers: [BsModalService],
-      imports: [MatIconModule],
+      imports: [MatIconModule, HttpClientTestingModule],
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(TeachersListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    fixture.whenStable().then(() => {
+      expect(component).toBeTruthy();
+    });
   });
 });
