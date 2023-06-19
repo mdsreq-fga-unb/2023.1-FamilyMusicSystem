5561d4fecbbc66015bed9edf69efc39749126513
Davi Ranieri Fonsêca <86808578+davirany@users.noreply.github.com>
2023-06-18 23:05:38 -0300

    revert: revertendo merges

--- frontend/src/app/modules/student/students-list/students-list.component.ts
+++ frontend/src/app/modules/student/students-list/students-list.component.ts
@@ -16,9 +16,7 @@ import { CookieService } from '../../../services/cookie.service';
 import { ConfirmationComponent } from '../../../shared/confirmation/confirmation.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatDialogRef } from '@angular/material/dialog';
-<<<<<<< HEAD
 import { PreloaderComponent } from '../../preloader/preloader.component';
-import { DataSharingService } from '../../../services/data-sharing.service';
 
 class Entry<T> {
   id: number;
@@ -35,6 +33,7 @@ class Response {
   styleUrls: ['./students-list.component.scss'],
 })
 export class StudentsListComponent implements OnInit {
+  public loading = true;
   public showAlertEdit = false;
   public showAlertDelete = false;
   public showAlertAdd = false;
@@ -52,8 +51,7 @@ export class StudentsListComponent implements OnInit {
     private http: HttpClient,
     private cookieService: CookieService,
     private fb: FormBuilder,
-    private dialog: MatDialog,
-    private dataSharingService: DataSharingService
+    private dialog: MatDialog
   ) { }
 
   headers() {
@@ -65,6 +63,8 @@ export class StudentsListComponent implements OnInit {
   }
 
   getStudent(args?: string) {
+    this.loading = true; // Define o estado de loading como true antes de fazer a requisição
+
     this.students$ = this.http
       .get<Response>(
         args ? `${this.prefixoUrlStudent}${args}` : this.prefixoUrlStudent,
@@ -81,7 +81,17 @@ export class StudentsListComponent implements OnInit {
           response.data.map((student) => student.attributes)
         )
       );
+
+    this.students$.subscribe(
+      () => {
+        this.loading = false;
+      },
+      () => {
+        this.loading = false;
+      }
+    );
   }
+
 
   search() {
     this.getStudent(
@@ -104,7 +114,7 @@ export class StudentsListComponent implements OnInit {
     return of();
   }
 
-  modalAddAlunos() {
+  modalNewAlunos() {
     const modalConfig = {
       backdrop: true,
       ignoreBackdropClick: false,
@@ -117,18 +127,14 @@ export class StudentsListComponent implements OnInit {
     );
     this.bsModalRef.onHide?.subscribe(() => {
       this.getStudent();
-      console.log(this.dataSharingService.ifshowAlertAdd);
-      if (this.dataSharingService.ifshowAlertAdd) {
-        this.showAlertAdd = true;
-        setTimeout(() => {
-          this.showAlertAdd = false;
-          this.dataSharingService.ifshowAlertAdd = false;
-        }, 3000);
-      }
+      this.showAlertAdd = true;
+      setTimeout(() => {
+        this.showAlertAdd = false;
+      }, 3000);
     });
   }
 
-  modalEditAlunos(student: Student, edit: boolean) {
+  modalAlunos(student: Student, edit: boolean) {
     const modalConfig = {
       backdrop: true,
       ignoreBackdropClick: false,
@@ -144,13 +150,10 @@ export class StudentsListComponent implements OnInit {
     );
     this.bsModalRef.onHide?.subscribe(() => {
       this.getStudent();
-      if (this.dataSharingService.ifshowAlertEdit) {
-        this.showAlertEdit = true;
-        setTimeout(() => {
-          this.showAlertEdit = false;
-          this.dataSharingService.ifshowAlertEdit = false;
-        }, 3000);
-      }
+      this.showAlertEdit = true;
+      setTimeout(() => {
+        this.showAlertEdit = false;
+      }, 3000);
     });
   }
 
