9ff18ae1f4acddfefdd1137d8d5a1d1d4708dfe2
maykonjuso <maykola1331@gmail.com>
2023-07-05 22:58:28 -0300

    fix: corrigindo bugs de responsividade

--- frontend/src/app/template/header/header.component.ts
+++ frontend/src/app/template/header/header.component.ts
@@ -1,19 +1,19 @@
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ContractComponent } from '../../modules/settings/contract/contract.component';
-import { PrivacyComponent } from '../../modules/settings/privacy/privacy.component';
-import { ResourcesComponent } from '../../modules/settings/resources/resources.component';
-import { AboutComponent } from '../../modules/settings/about/about.component';
-import { HelpComponent } from '../../modules/settings/help/help.component';
-import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
-import { Component, HostListener, OnInit } from '@angular/core';
-import { StudentsViewComponent } from '../../modules/student/students-view/students-view.component';
-import { CookieService } from '../../services/cookie.service';
-import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { ContractComponent } from "../../modules/settings/contract/contract.component";
+import { PrivacyComponent } from "../../modules/settings/privacy/privacy.component";
+import { ResourcesComponent } from "../../modules/settings/resources/resources.component";
+import { AboutComponent } from "../../modules/settings/about/about.component";
+import { HelpComponent } from "../../modules/settings/help/help.component";
+import { BsDropdownConfig } from "ngx-bootstrap/dropdown";
+import { Component, HostListener, OnInit } from "@angular/core";
+import { StudentsViewComponent } from "../../modules/student/students-view/students-view.component";
+import { CookieService } from "../../services/cookie.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
   providers: [
     {
       provide: BsDropdownConfig,
@@ -22,8 +22,9 @@ import { Router } from '@angular/router';
   ],
 })
 export class HeaderComponent implements OnInit {
-  icon = ['brightness_2', 'wb_sunny'];
-  icon_now = 'brightness_2';
+  icon = ["brightness_2", "wb_sunny"];
+  icon_now = "brightness_2";
+  small = false;
   private bsModalRef: BsModalRef;
 
   constructor(
@@ -37,7 +38,7 @@ export class HeaderComponent implements OnInit {
       backdrop: true,
       ignoreBackdropClick: false,
       initialState: {},
-      class: 'modal-lg',
+      class: "modal-lg",
     };
     this.bsModalRef = this.modalService.show(HelpComponent, modalConfig);
     this.bsModalRef.onHide?.subscribe(() => {});
@@ -47,7 +48,7 @@ export class HeaderComponent implements OnInit {
       backdrop: true,
       ignoreBackdropClick: false,
       initialState: {},
-      class: 'modal-lg',
+      class: "modal-lg",
     };
     this.bsModalRef = this.modalService.show(
       StudentsViewComponent,
@@ -61,7 +62,7 @@ export class HeaderComponent implements OnInit {
       backdrop: true,
       ignoreBackdropClick: false,
       initialState: {},
-      class: 'modal-lg',
+      class: "modal-lg",
     };
     this.bsModalRef = this.modalService.show(ContractComponent, modalConfig);
     this.bsModalRef.onHide?.subscribe(() => {});
@@ -71,7 +72,7 @@ export class HeaderComponent implements OnInit {
       backdrop: true,
       ignoreBackdropClick: false,
       initialState: {},
-      class: 'modal-lg',
+      class: "modal-lg",
     };
     this.bsModalRef = this.modalService.show(PrivacyComponent, modalConfig);
     this.bsModalRef.onHide?.subscribe(() => {});
@@ -81,7 +82,7 @@ export class HeaderComponent implements OnInit {
       backdrop: true,
       ignoreBackdropClick: false,
       initialState: {},
-      class: 'modal-lg',
+      class: "modal-lg",
     };
     this.bsModalRef = this.modalService.show(ResourcesComponent, modalConfig);
     this.bsModalRef.onHide?.subscribe(() => {});
@@ -91,14 +92,14 @@ export class HeaderComponent implements OnInit {
       backdrop: true,
       ignoreBackdropClick: false,
       initialState: {},
-      class: 'modal-lg',
+      class: "modal-lg",
     };
     this.bsModalRef = this.modalService.show(AboutComponent, modalConfig);
     this.bsModalRef.onHide?.subscribe(() => {});
   }
 
   toggle() {
-    const theme = document.body.classList.toggle('darkTheme');
+    const theme = document.body.classList.toggle("darkTheme");
 
     if (theme) {
       return (this.icon_now = this.icon[1]);
@@ -109,7 +110,7 @@ export class HeaderComponent implements OnInit {
   screenSize: number;
   iscomp = true;
 
-  @HostListener('window:resize', [])
+  @HostListener("window:resize", [])
   onResize() {
     const screenWidth = window.innerWidth;
     this.screenSize = screenWidth;
@@ -117,6 +118,11 @@ export class HeaderComponent implements OnInit {
       this.iscomp = true;
     } else {
       this.iscomp = false;
+    }
+    if (this.screenSize <= 400) {
+      this.small = true;
+    } else {
+      this.small = false;
     }
   }
 
@@ -128,10 +134,15 @@ export class HeaderComponent implements OnInit {
     } else {
       this.iscomp = false;
     }
+    if (this.screenSize <= 400) {
+      this.small = true;
+    } else {
+      this.small = false;
+    }
   }
 
   logout(): void {
-    this.cookieService.deleteCookie('jwt');
-    this.router.navigate(['/']);
+    this.cookieService.deleteCookie("jwt");
+    this.router.navigate(["/"]);
   }
 }
