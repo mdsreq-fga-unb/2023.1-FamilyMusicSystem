732204c6f23853534e4a02b02fd80bf502b1ecb7
Luan Mateus <luanmateus.duarte@gmail.com>
2023-07-10 18:10:28 -0300

    update: finalização do frontend da página de mural

--- frontend/src/app/modules/murals/mural-view/mural-view.component.spec.ts
+++ frontend/src/app/modules/murals/mural-view/mural-view.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MuralViewComponent } from './mural-view.component';
+import { BsModalService } from "ngx-bootstrap/modal";
+import { MatIconModule } from "@angular/material/icon";
+import { HttpClient, HttpHandler } from "@angular/common/http";
+import { Room } from "../../../models/room";
+import { Mural } from 'src/app/models/mural';
 
 describe('MuralViewComponent', () => {
   let component: MuralViewComponent;
   let fixture: ComponentFixture<MuralViewComponent>;
 
   beforeEach(async () => {
+    const mural: Mural = new Mural();
     await TestBed.configureTestingModule({
-      declarations: [ MuralViewComponent ]
+      declarations: [ MuralViewComponent ],
+      providers: [BsModalService, HttpClient, HttpHandler],
+      imports: [MatIconModule],
     })
     .compileComponents();
 
--- frontend/src/app/modules/murals/mural-view/mural-view.component.ts
+++ frontend/src/app/modules/murals/mural-view/mural-view.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Mural } from "src/app/models/mural";
+import { CookieService } from "./../../../services/cookie.service";
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { BsModalRef } from "ngx-bootstrap/modal";
+import { Subject } from "rxjs";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { FormValidations } from "../../../shared/form-validations";
+import { DataSharingService } from "../../../services/data-sharing.service";
 
 @Component({
   selector: 'app-mural-view',
@@ -6,5 +19,116 @@ import { Component } from '@angular/core';
   styleUrls: ['./mural-view.component.scss']
 })
 export class MuralViewComponent {
+  public mural: Mural;
+  room$: Observable<Mural[]> | undefined;
+  public muralForm: FormGroup;
+  public muralValid: boolean = false;
+  public showAlert: boolean = false;
+  public onClose: Subject<boolean>;
+  public edicao = false;
+  public nome: string;
+  public edit = false;
+  public inicial = true;
+  public isFormValid = false;
+  public dataAtual: string;
+  public loading: boolean = false;
+  public file: File;
+  public baseUrl = `https://20231-familymusicsystem-production.up.railway.app`;
+  public canEdit: boolean = false;
 
+  error: any | undefined;
+  constructor(
+    private bsModalRef: BsModalRef,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef,
+    private cookieService: CookieService,
+    private dataSharingService: DataSharingService
+  ) {
+    this.dataAtual = new Date().toISOString().split("T")[0];
+  }
+
+  getHeaders(): HttpHeaders {
+    const jwt = this.cookieService.getCookie("jwt");
+    const headers = new HttpHeaders().set("Authorization", `Bearer ${jwt}`);
+    return headers;
+  }
+
+  headers() {
+    const jwt = this.cookieService.getCookie("jwt") || "";
+    const isTokenValid = this.cookieService.isTokenValid(jwt);
+    if (!isTokenValid) {
+      console.log("erro");
+    }
+    let headers = new HttpHeaders();
+    headers = headers.append("Authorization", `Bearer ${jwt}`);
+    const opts = { headers: headers, params: { populate: "*" } };
+    return opts;
+  }
+
+  ngOnInit(): void {
+    this.muralForm = this.fb.group({
+      titleMural: [
+        { value: this.mural.Title, disabled: !this.edit },
+        Validators.required,
+      ],
+      muralMessage: [
+        { value: this.mural.Message, disabled: !this.edit },
+        Validators.required,
+      ],
+    });
+    this.cdr.detectChanges();
+    this.muralForm.updateValueAndValidity();
+    this.muralForm.statusChanges.subscribe(() => {
+      this.isFormValid = this.muralForm.valid;
+    });
+  }
+
+  onEdit($mural: Mural): void {
+    const baseUrl = `https://20231-familymusicsystem-production.up.railway.app`;
+    const mural: Mural = new Mural();
+    const headers = this.getHeaders();
+    const requestOptions = { headers };
+
+    mural.Title = this.muralForm.get("titleMural")?.value.toString();
+    mural.Message = this.muralForm.get("MuralMessage")?.value.toString();
+
+    const body = {
+      data: mural,
+    };
+
+
+  }
+
+  transformFirstLetterToUppercase(inputElement: HTMLInputElement) {
+    const value = inputElement.value;
+    if (value.length > 0) {
+      const words = value.toLowerCase().split(" ");
+      const excludedWords = ["de", "des", "do", "dos", "das", "da", "e"];
+      const result = words.map((word, index) => {
+        if (index === 0 || !excludedWords.includes(word)) {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        } else {
+          return word;
+        }
+      });
+      inputElement.value = result.join(" ");
+    }
+  }
+
+  scrollTop() {
+    const div = document.getElementById("scroll");
+    if (div !== null) {
+      div.scrollTop = 0;
+    }
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    this.error = error.message;
+    return of();
+  }
+
+  sair() {
+    this.bsModalRef.hide();
+  }
 }
